66e20db18bde8c21e5322684d163f670635d3f80
unknown <shanzidadisha1234@gmail.com>
2024-07-12 19:15:40 +0600

    private route added

--- src/Hooks/PrivateRoute.jsx
+++ src/Hooks/PrivateRoute.jsx
@@ -1,25 +1,19 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import Loading from "../Component/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
   if (loading) {
-    return (
-        <div className="container mx-auto mt-10 text-center">
-        <span className="loading loading-ring loading-lg"></span>
-      </div>
-    );
+    return <Loading />;
   }
   if (user) {
     return children;
   }
   return (
-    <Navigate
-      state={{ redirectTo: location.pathname }}
-      to="/login"
-    ></Navigate>
+    <Navigate state={{ redirectTo: location.pathname }} to="/"></Navigate>
   );
 };
 

094d4ed7eaff1d8619f4ab27a3966047d599a657
unknown <shanzidadisha1234@gmail.com>
2024-07-16 17:26:58 +0600

    student table design

--- src/Component/Dashboard/StudentOfMco.jsx
+++ src/Component/Dashboard/StudentOfMco.jsx
@@ -5,9 +5,10 @@ import { useParams } from "react-router-dom";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { FaCheck } from "react-icons/fa6";
 import { TbLocationBroken } from "react-icons/tb";
+import StudentForAdmin from "./StudentForAdmin";
 
 export default function StudentOfMco() {
-  const {  email } = useParams();
+  const { email } = useParams();
   const axiosPublic = useAxiosPublic();
 
   const {
@@ -26,50 +27,60 @@ export default function StudentOfMco() {
     },
   });
 
-  console.log(allStudentofMco)
-
   const enrollmentCount = useMemo(() => {
     if (!allStudentofMco) return 0;
-    return allStudentofMco.filter(student => student.status.status === 'enrollment').length;
+    return allStudentofMco.filter(
+      (student) => student.status.status === "enrollment"
+    ).length;
   }, [allStudentofMco]);
 
   const dropoutCount = useMemo(() => {
     if (!allStudentofMco) return 0;
-    return allStudentofMco.filter(student => student.status.status === 'dropout').length;
+    return allStudentofMco.filter(
+      (student) => student.status.status === "dropout"
+    ).length;
   }, [allStudentofMco]);
 
-
-
+  console.log(allStudentofMco)
 
   return (
-    <div className="grid justify-between gap-2 items-center grid-cols-3">
-      <div className="bg-white rounded-md drop-shadow-md p-3 flex gap-5 items-center" >
-        <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
-          <PiStudent className="text-4xl"/>
-        </div>
-        <div >
-            <h1 className="text-xl font-semibold text-customPurple">Total Students</h1>
+    <div>
+      <div className="grid justify-between gap-2 items-center grid-cols-3">
+        <div className="bg-white rounded-md drop-shadow-md p-3 flex gap-5 items-center">
+          <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
+            <PiStudent className="text-4xl" />
+          </div>
+          <div>
+            <h1 className="text-xl font-semibold text-customPurple">
+              Total Students
+            </h1>
             <h1 className="text-xl text-gray-700">{allStudentofMco?.length}</h1>
+          </div>
         </div>
-      </div>
-      <div className="bg-white rounded drop-shadow-md p-3 flex gap-5 items-center" >
-        <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
-          <FaCheck  className="text-4xl"/>
-        </div>
-        <div >
-            <h1 className="text-xl font-semibold text-customPurple">Enrolled Students</h1>
+        <div className="bg-white rounded drop-shadow-md p-3 flex gap-5 items-center">
+          <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
+            <FaCheck className="text-4xl" />
+          </div>
+          <div>
+            <h1 className="text-xl font-semibold text-customPurple">
+              Enrolled Students
+            </h1>
             <h1 className="text-xl text-gray-700">{enrollmentCount}</h1>
+          </div>
         </div>
-      </div>
-      <div className="bg-white rounded drop-shadow-md p-3 flex gap-5 items-center" >
-        <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
-          <TbLocationBroken className="text-4xl"/>
-        </div>
-        <div >
-            <h1 className="text-xl font-semibold text-customPurple">Dropout Students</h1>
+        <div className="bg-white rounded drop-shadow-md p-3 flex gap-5 items-center">
+          <div className="flex justify-center items-center bg-customPurple p-4 text-white rounded-md">
+            <TbLocationBroken className="text-4xl" />
+          </div>
+          <div>
+            <h1 className="text-xl font-semibold text-customPurple">
+              Dropout Students
+            </h1>
             <h1 className="text-xl text-gray-700">{dropoutCount}</h1>
+          </div>
         </div>
       </div>
+      <StudentForAdmin allStudentofMco={allStudentofMco} isLoading={isLoading}/>
     </div>
   );
 }
